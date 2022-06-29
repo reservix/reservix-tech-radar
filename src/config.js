3caf8964df2dc2b1313955ef12f4d8c0ec6d295b
Sebastian Sebald <sebastian.sebald@reservix.de>
2022-06-29 16:36:46 +0200

    make the radar reponsive

--- src/config.js
+++ src/config.js
@@ -4,6 +4,10 @@ import { quadrants, entries } from './quadrants';
 radar_visualization({
   svg_id: 'radar',
   title: 'Reservix Tech Radar — 2022 Q3',
+  /**
+   * Don't change these numbers, we make the
+   * radar responsive via CSS
+   */
   width: 1500,
   height: 1000,
   colors: {
