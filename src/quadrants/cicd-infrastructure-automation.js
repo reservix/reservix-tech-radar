dc86285ed60d161700e5a3d433a7cc9bf2e240c6
Sebastian Sebald <sebastian.sebald@gmail.com>
2022-06-29 13:29:28 +0200

    added more stuff

--- src/quadrants/cicd-infrastructure-automation.js
+++ src/quadrants/cicd-infrastructure-automation.js
@@ -5,4 +5,21 @@ export const quadrant = 3;
 
 export const name = 'CI/CD, Infrastructure & Automation';
 
-export const entries = [];
+export const entries = [
+  {
+    label: 'Docker',
+    link: 'https://www.docker.com/',
+    quadrant,
+    ring: adopt,
+    active: true,
+    moved: 0,
+  },
+  {
+    label: 'GitLab CI',
+    link: 'https://docs.gitlab.com/ee/ci/',
+    quadrant,
+    ring: adopt,
+    active: true,
+    moved: 0,
+  },
+];
